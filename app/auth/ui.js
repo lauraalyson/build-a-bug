03fa7bf8d562b72412fd8c3f93dacc1dfda11ac0
lauraalyson <lauraalyson3@gmail.com>
2021-08-22 12:14:40 -0400

    remove 'hide bug cage'

--- app/auth/ui.js
+++ app/auth/ui.js
@@ -33,7 +33,6 @@ const onSignInSuccess = (response) => {
   $('#create-bug').hide()
   $('#bug-cage').show()
   $('#show-sign-in').hide()
-  $('#bug-message').hide()
 }
 
 const onSignInFailure = () => {
